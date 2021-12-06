10cd2744ad4b08b2eed54b4611ae16ed5c9f3ec3
Juda.Barnes <Juda.Barnes@Ericom.com>
2021-12-06 10:55:50 +0200

    Fix to Compilation script and Work Directly with RAM API

--- files/ram-webserver.py
+++ files/ram-webserver.py
@@ -1,90 +1,86 @@
 #!/usr/bin/env python3
+#
+#
+# 
+# Juda Barnes (c) - 06/12/2021 
+# Purpose of this script is to get data from RAM and Morty API interface
+# Gather filtered data according specific filter and then pulish the data
+# As API,
+# In order to run the script you must use flask
+# As the following example 
+# /usr/local/bin/flask run--host=0.0.0.0
+#
+# otherwise if you use python3 to run the code it will not server as WebServer
+# And will only show the chracters that are being added to the list
+#
+#
+
 
 import json
 import pprint
-
-
-# rather than use request directly to the API interface
-# i found Rampi python library which provide easy to use interface
-# installation and documentation can be find here 
-# https://ramapi.readthedocs.io/en/latest/installation.html#
-
-import ramapi
-from ramapi import Base
-from ramapi import Character
-
-
-# this library use for Python web server 
+import requests
+import sys
 from flask import Flask, request, jsonify
 
-
-# Init Variable:
-juda1 = None
-
-# Main:
-ramapi.Base.api_info()
-ramapi.Base.schema()
-
-RamCharacterFiltered = ramapi.Character.filter(species='human', status='alive')
-
-# Additional examples how to use the Library 
-#juda1= ramapi.Character.get_all()
-##amapi.Character.filter(species='human',status='alive')
-#juda2 = ramapi.Location(4)
-#ramapi.Episode([10,28]) //Takes list as parameter
-
-
-# Print the list for myself in order to verify it own data
-#pprint.pprint (juda1)
-
+# array  that will characters according the filter Specie=C-137, Humanhat will
+# use for Webserver
 WebserverData = [] 
-for x in RamCharacterFiltered['results']:
-    newEntry = {}
-    newEntry['name']=x['name']
-    newEntry['location']=x['location']['name']
-    newEntry['image']=x['image']
-    WebserverData.append(newEntry)
-    #    print(x['status'])
-    # print (x['species'])
-
-#pprint.pprint(WebserverData)
 
 
+# Purpose of this function to send API request to RickAndMarty Website
+# And retrive Data via API,   in case of HTTP response rather than 200
+# the function will return None Values
+# function will return tupple, the data and the next Page for paging purposes
+def getRamPage(page = 1):
+  url = "https://rickandmortyapi.com/api/character/?status=alive&species=human&page="+str(page)
+  APIResult=requests.get(url)
+  if ( APIResult.status_code!=200 ):
+      print("API Return bad response")
+      return None , None
+  data = APIResult.json()
+  return data['results'],data['info']['next']
 
-#TODO:  If Webserver Data is empty then exit with a failure
+
+
+# Initializing Variables
+Data = []
+NextPage = None
+PageNumber=1
+# Counter for characters we skiped 
+TotalSkip = 0
+
+
+
 #
+# Getting Data from Website til Next is Not null
+Data, NextPage = getRamPage(PageNumber) 
+while ( Data is not None and NextPage is not None ):
+    # Adding the Data to List Array
+    for ItemToAdd  in Data:
+      # Check the origin of the character
+      if ItemToAdd['origin']['name']!="Earth (C-137)":
+          newEntry = {}
+          newEntry['name']=ItemToAdd['name']
+          newEntry['location']=ItemToAdd['location']['name']
+          print ("Adding character name %s from %s to List listCount: %d" % (newEntry['name'],newEntry['location'],len(WebserverData)) )
+          newEntry['image']=ItemToAdd['image']
+          WebserverData.append(newEntry)
+      else:
+          print ("Skip character %s from %s" % (ItemToAdd['name'],ItemToAdd['origin']['name']))
+          TotalSkip += 1
+    # Getting the next Page
+    PageNumber=PageNumber + 1 
+    Data, NextPage = getRamPage(PageNumber) 
 
-# Now handling Webserver
-# I use this link 
-# as a guide for flask https://realpython.com/api-integration-in-python/
 
-app = Flask(__name__)
+# Print Counters
+print ("Total in List {} Total Skip {}".format(len(WebserverData),TotalSkip))
 
-countries = [
-    {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
-    {"id": 2, "name": "Australia", "capital": "Canberra", "area": 7617930},
-    {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
-]
-
-def _find_next_id():
-    return max(country["id"] for country in countries) + 1
-
-@app.get("/countries")
-def get_countries():
-    return jsonify(countries)
-
-
-@app.get("/ram")
-def get_ram():
+if len(WebserverData)<1:
+  print ("List contain no data aborting")
+  exit(1)
+else:
+  app = Flask(__name__)
+  @app.get("/ram")
+  def get_ram():
     return jsonify(WebserverData)
-
-
-
-@app.post("/countries")
-def add_country():
-    if request.is_json:
-        country = request.get_json()
-        country["id"] = _find_next_id()
-        countries.append(country)
-        return country, 201
-    return {"error": "Request must be JSON"}, 415
