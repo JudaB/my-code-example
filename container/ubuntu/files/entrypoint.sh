aab09e777e63af9f2bef56a9fecffd2f6ec038c6
Juda Barnes <juda.barnes@gmail.com>
2023-07-15 07:30:19 +0000

    x

--- container/ubuntu/files/entrypoint.sh
+++ container/ubuntu/files/entrypoint.sh
@@ -6,14 +6,20 @@ echo "Execute EntryPoint"
 echo $#
 echo "$@"
 
-if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
-	uname -a
-	if [ -f /tmp/zip_job.py ] ; then
-	    	echo "[INFO] Zip Exists"
-	else
-   		echo "[CRITICAL] Zip does Not Exists"
-	fi
+uname -a
+if [ -f /tmp/zip_job.py ] ; then
+    	echo "[INFO] Zip Exists"
+else
+	echo "[CRITICAL] Zip does Not Exists"
+	exit 1
 fi
 
-echo "Executing" "$@"
-exec "$@"
+# This is use to allow docker-plugin
+# to execute cat this way it hold the container run
+# and jenkins is runing within the container
+
+if [ "$#" -gt 0 ]; then
+	echo "Executing" "$@"
+	exec "$@"
+fi
+
