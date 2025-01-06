133738c207b8f538a347ad0d6b684e39139fa25f
Juda Barnes <juda.barnes@gmail.com>
2025-01-06 22:35:50 +0200

    x

--- container/rampi/_compile.sh
+++ container/rampi/_compile.sh
@@ -3,28 +3,13 @@
 # Compilation script
 # Will handle the following
 #	Docker Build
-#	Unitest
 #	Upload
 #
-#  Use QUITE_COMPILE   if you do not want to reduce verbose outputs
-#                      or comment it out
-#
 
-IMAGE="judab/my-docker-example"
-QUITE_COMPILE=FALSE
-
-
-
-
+IMAGE="judab/backend-rampi"
 
 docker_build () {
-#Build Image
-if [ -z ${QUITE_COMPILE} ] ; then 
-	docker build -t ${IMAGE}:latest . 
-else
-	docker build -t ${IMAGE}:latest .  >/dev/null 2>&1
-fi
-
+docker build -t ${IMAGE}:latest . 
 if [[ $? -ne 0 ]] ; then 
     echo "[ERROR]: Docker Build to ${IMAGE}  Failed"
     exit 2
@@ -35,41 +20,14 @@ fi
 
 
 
-
-docker_unitest () {
-# Unitest
-CONTAINER_ID=$(docker run -td docker.io/${IMAGE}:latest)
-sleep 10
-docker logs $CONTAINER_ID  | grep  "Running on" >/dev/null 2>&1
-RT=$?
-docker stop $CONTAINER_ID
-
-echo "MY RT: $RT"
-#> /dev/null
-if [[ $? -ne 0 ]] ; then 
-    echo "[ERROR]: Docker unitest  to ${IMAGE}  Failed"
-    exit 2
-else 
-    echo "[INFO]: Docker unitest  to ${IMAGE} Pass sucessfully"
-fi
-}
-
-
-
 docker_upload () {
 TAG=`date +"%y%m%d-%H.%M"`
-
 docker tag "${IMAGE}:latest" "${IMAGE}:"$TAG
-if [ -z ${QUITE_COMPILE} ] ; then 
-	docker push "${IMAGE}:$TAG"
-else
-	docker push "${IMAGE}:$TAG" > /dev/null 2>&1
-fi
-
+docker push "${IMAGE}:$TAG"
 if [[ $? -ne 0 ]] ; then 
     echo "[ERROR]: Docker Push Failed"
     exit 2
-else 
+else
     echo "[INFO]: Docker Push ${IMAGE}:${TAG} Pass sucessfully"
 fi
 }
@@ -78,7 +36,6 @@ fi
 
 # Main
 docker_build
-docker_unitest
 docker_upload
 
 
